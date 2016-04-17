d436ee1cb82b8dcf0965f6a9548ee7d4dbe06919
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-17 09:49:01 +0200

    Create receiver.py

---
+++ receiver.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from pprint import pprint
+import json
+
+app = Flask(__name__)
+
+#Added this k
+#port=os.environ['PORTK']
+
+@app.route('/log', methods=['POST'])
+def log():
+    try:
+        data_to_log = json.loads(request.json)
+        pprint(data_to_log)
+    except Exception as e:
+        print(e)
+        raise(e)
+    return(jsonify({'result':'ok'}))
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=8080, debug=True)
